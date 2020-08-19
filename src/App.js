3911d2475167f6c81722a2c22331dcf57597beed
andrelrpacheco <andrelrpacheco@gmail.com>
2020-08-19 20:41:08 -0300

    CRUD de Repositorios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
 
-import "./styles.css";
+import './styles.css'
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+	const [repositorys, setRepositorys] = useState([])
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	useEffect(() => {
+		api.get('repositories').then((response) => {
+			setRepositorys(response.data)
+		})
+	}, [])
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+	async function handleAddRepository() {
+		const response = await api.post('repositories', {
+			title: `Novo repositório ${Date.now()}`,
+			url: 'https://github.com/andrelrpacheco/',
+			techs: ['React, Node, React-Native TypeScript']
+		})
+		const repository = response.data
+		setRepositorys([...repositorys, repository])
+	}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+	async function handleRemoveRepository(id) {
+		const response = await api.delete(`repositories/${id}`)
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+		setRepositorys(repositorys.filter((repository) => repository.id !== id))
+	}
+
+	return (
+		<div>
+			<ul data-testid='repository-list'>
+				{repositorys.map((repository) => {
+					return (
+						<li key={repository.id}>
+							<ul>
+								<li>
+									<a href={repository.url} target='_blank'>
+										{repository.title}
+									</a>
+								</li>
+								<li>{repository.likes}</li>
+							</ul>
+							<button onClick={() => handleRemoveRepository(repository.id)}>
+								Remover
+							</button>
+						</li>
+					)
+				})}
+			</ul>
+			<button onClick={handleAddRepository}>Adicionar</button>
+		</div>
+	)
 }
 
-export default App;
+export default App
